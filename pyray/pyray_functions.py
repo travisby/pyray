f6ee5b2f3231c5ffd00dc421af8817fe7bca3118
Travis Beatty <travisby@gmail.com>
2013-01-18 08:16:20 -0500

    Added PHP docstring to array_keys

--- pyray/pyray_functions.py
+++ pyray/pyray_functions.py
@@ -2,6 +2,12 @@
 import pyray
 
 def array_keys(array, search_value=None):
+    """array_keys() returns the keys, numeric and string, from the input array.
+
+    If the optional search_value is specified, then only the keys for that
+    value are returned. Otherwise, all the keys from the input are returned.
+    """
+
     if not search_value:
         return array.keys()
 
