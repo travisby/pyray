3430b322305e6e3a7b88348ad5f7b8c121f56af3
Travis Beatty <travisby@gmail.com>
2013-01-18 08:21:02 -0500

    Updated array_keys() to use our new extend method, and added functionallity for the search_value

--- pyray/pyray_functions.py
+++ pyray/pyray_functions.py
@@ -7,7 +7,11 @@ def array_keys(array, search_value=None):
     If the optional search_value is specified, then only the keys for that
     value are returned. Otherwise, all the keys from the input are returned.
     """
+    keys = pyray.Pyray()
 
     if not search_value:
-        return array.keys()
+        keys.extend(array.keys())
+    else:
+        keys.extend([x[0] for x in array.items() if [1] == search_value])
+    return keys
 
