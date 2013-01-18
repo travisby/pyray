0b5c6cb8fa9f9707935c0e0c6837f8a7600626a9
Travis Beatty <travisby@gmail.com>
2013-01-18 00:51:06 -0500

    Added a string representation to our object

--- pyray/pyray.py
+++ pyray/pyray.py
@@ -11,6 +11,18 @@ class PyRay:
         """Sets our data members to their default state"""
         self._obj = collections.OrderedDict()
 
+    def __repr__(self):
+        header = 'PyRay(%(len)s) {' % {'len': len(self)}
+        body = ''.join([self._repr_body(item) for item in self._obj.items()])
+        return '%s %s \n }' % (header, body)
+
+
+    def _repr_body(self, item):
+        return (
+            '\n\t["%(key)s"]=>\n\t%(type)s %(val)s' %
+            {'key': item[0], 'type': type(item[1]), 'val': item[1]}
+        )
+
     def __len__(self):
         """What we interpret our size to be"""
         return len(self._obj.keys())
